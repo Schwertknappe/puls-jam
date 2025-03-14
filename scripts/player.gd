619373a24177dc5f8c0a12f86b83e5288c3d78b5
Schwertknappe <alnolte@web.de>
2025-03-14 21:50:27 +0100

    added player animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -8,6 +8,8 @@ const JUMP_VELOCITY = -400.0
 
 var double_jump_available = true
 var last_velocity
+@onready var sprite : Sprite2D = $Sprite2D
+@onready var animation_player : AnimationPlayer = $AnimationPlayer
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -31,5 +33,17 @@ func _physics_process(delta):
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+	
+	_handle_animation(delta, direction)
+	
 	move_and_slide()
+
+
+func _handle_animation(delta, direction):
+	if direction != 0.0 and velocity.x < 0.0 != sprite.flip_h:
+		sprite.flip_h = !sprite.flip_h
+	
+	if velocity.x != 0.0:
+		animation_player.play("walk")
+	elif is_on_floor() and animation_player.is_playing():
+		animation_player.pause()
