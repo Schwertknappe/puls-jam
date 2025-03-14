b5f558cf580dee735ab2c51d96bb34b5f29037bf
Schwertknappe <106437992+Schwertknappe@users.noreply.github.com>
2025-03-14 22:52:24 +0100

    fix merge conflicts

--- scripts/path_follow_2d.gd
+++ scripts/path_follow_2d.gd
@@ -1,6 +1,9 @@
-extends PathFollow2D
+extends Path2D
 
 @export var speed : float = 1
+var direction = 1
+
+@onready var path : PathFollow2D = $PathFollow2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,5 +12,9 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	progress_ratio += speed * delta
+	var new_value = clamp(path.progress_ratio + speed * delta * direction, 0.0, 1.0)
 	
+	if new_value == 1.0 or new_value == 0.0:
+		direction *= -1
+		
+	path.progress_ratio = new_value
