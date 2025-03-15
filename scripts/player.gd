7c0a8fd82440ceb3683ff0156eb00d625b110583
Schwertknappe <106437992+Schwertknappe@users.noreply.github.com>
2025-03-15 16:07:32 +0100

    spikes and ladders

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,6 +9,7 @@ signal hit
 @export var DOUBLE_JUMP_VELOCITY = -300.0
 @export var WALLJUMP_VELOCITY = Vector2(200.0, -300.0)
 @export_range(0.0, 1.0, 0.05) var wall_cling_modifier = 0.4
+@export var climb_speed = 100.0
 
 @export_category("Breakable Mechanics")
 @export var jump_enabled : bool = true
@@ -24,7 +25,7 @@ signal hit
 @export var can_walk_left: bool = true
 @export var can_walk_right: bool = true
 
-enum State {IDLE, WALKING, JUMPING, LANDING}
+enum State {IDLE, WALKING, JUMPING, LANDING, CLIMBING}
 
 var current_speed = SPEED
 var double_jump_available = false
@@ -33,6 +34,8 @@ var start_position
 var state : State = State.IDLE
 var input_enabled : bool = true
 var lives_left = MAX_LIVES
+var can_climb : bool = false
+var current_gravity : Vector2
 
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
@@ -50,14 +53,18 @@ func _ready():
 	if not collides_with_walls:
 		collision_layer &= ~(1 << 2)
 		collision_mask &= ~(1 << 2)
+	
+	current_gravity = get_gravity()
 
 func _physics_process(delta):
+	current_gravity = Vector2(0.0,0.0) if state == State.CLIMBING else get_gravity()
+	
 	# Add the gravity.
 	if not is_on_floor():
 		if not clinging_to_wall:
-			velocity += get_gravity() * delta * gravity_modifier
+			velocity += current_gravity * delta * gravity_modifier
 		else: 
-			velocity += get_gravity() * delta * gravity_modifier * wall_cling_modifier
+			velocity += current_gravity * delta * gravity_modifier * wall_cling_modifier
 	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -81,12 +88,21 @@ func _physics_process(delta):
 		velocity.x += direction * current_speed
 		velocity.x = clamp(velocity.x, -current_speed, current_speed)
 	else:
-		if is_on_floor():
+		if is_on_floor() or state == State.CLIMBING:
 			velocity.x = move_toward(velocity.x, 0, current_speed)
 		elif velocity.x > 0.0:
 			velocity.x = move_toward(velocity.x, current_speed/2, current_speed)
 		elif velocity.x < 0.0:
 			velocity.x = move_toward(velocity.x, -current_speed/2, current_speed)
+	
+	var v_direction = Input.get_axis("move_up", "move_down")
+	if v_direction and can_climb:
+		state = State.CLIMBING
+		if double_jump_enabled != double_jump_available:
+			double_jump_available = double_jump_enabled
+		velocity.y = v_direction * climb_speed
+	elif state == State.CLIMBING:
+		velocity.y = 0.0
 	
 	# check if player just landed and reset double jump
 	if is_on_floor() and state == State.JUMPING:
@@ -102,7 +118,7 @@ func _physics_process(delta):
 
 
 func _can_jump() -> bool:
-	return jump_enabled and (is_on_floor() or wall_check.is_colliding() or (!is_on_floor() and double_jump_available))
+	return jump_enabled and (is_on_floor() or wall_check.is_colliding() or state == State.CLIMBING or (!is_on_floor() and double_jump_available))
 
 func _jump():
 	current_speed = SPEED
@@ -122,7 +138,7 @@ func _jump():
 		_flip_horizontally()
 	
 	# use double jump
-	if not is_on_floor() and not wall_check.is_colliding():
+	if not is_on_floor() and not wall_check.is_colliding() and state != State.CLIMBING:
 		double_jump_available = false
 		velocity.y = DOUBLE_JUMP_VELOCITY
 		animation_player.stop()
@@ -154,7 +170,7 @@ func _handle_animation(delta, direction):
 	if gravity_modifier < 0.0 != sprite.flip_v:
 		sprite.flip_v = ! sprite.flip_v
 	
-	if velocity.x != 0.0 and state != State.JUMPING and state != State.LANDING:
+	if velocity.x != 0.0 and state != State.JUMPING and state != State.LANDING and state != State.CLIMBING:
 		animation_player.play("walk")
 		state = State.WALKING
 	elif is_on_floor() and state != State.LANDING and state != State.JUMPING:
@@ -176,18 +192,40 @@ func respawn():
 	state = State.IDLE
 	velocity = Vector2(0.0,0.0)
 
-
-
-# SIGNALS
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
+func die():
 	hide() # Player disappears after being hit.
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
 	collider.set_deferred("disabled", true) # Replace with function body.
 	if lives_left > 0:
 		respawn()
+	else:
+		input_enabled = false
 
+func tile_has_property(tile_map : TileMapLayer, property : String) -> bool:
+	var tile_coords = tile_map.local_to_map(global_position)
+	var tile_data : TileData = tile_map.get_cell_tile_data(tile_coords)
+	if tile_data != null:
+		return tile_data.has_custom_data(property) and tile_data.get_custom_data(property) == true
+	return false
+
+
+
+# SIGNALS
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body is TileMapLayer:
+		if tile_has_property(body, "is_ladder"):
+			can_climb = true
+		elif tile_has_property(body, "is_damage"):
+			die()
+	else:
+		die()
+
+func _on_area_2d_body_exited(body):
+	if body is TileMapLayer and !tile_has_property(body, "is_ladder"):
+		can_climb = false
+		state = State.IDLE
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "land":
