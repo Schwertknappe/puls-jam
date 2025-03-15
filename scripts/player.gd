782a13a8b414dac9e38328892fae8937823d57fc
Schwertknappe <106437992+Schwertknappe@users.noreply.github.com>
2025-03-15 09:16:37 +0100

    finished player animation

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,67 +1,107 @@
 extends CharacterBody2D
 signal hit
 
-@export var can_jump : bool = true
-@export var can_double_jump : bool = true
+@export var jump_endabled : bool = true
+@export var double_jump_enabled : bool = true
+
+enum State {IDLE, WALKING, JUMPING, LANDING}
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-var double_jump_available = true
-var last_velocity
+var current_speed = SPEED
+var double_jump_available = false
+var start_position
+var state : State = State.IDLE
+var input_enabled : bool = true
+
 @onready var sprite : Sprite2D = $Sprite2D
 @onready var animation_player : AnimationPlayer = $AnimationPlayer
+@onready var collider : CollisionShape2D = $CollisionShape2D
+
+func _ready():
+	start_position = transform.get_origin()
+	if double_jump_enabled:
+		double_jump_available = true
 
 func _physics_process(delta):
+	$Label.text = "State: "+State.keys()[state]
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 	
 	# check if player just landed and reset double jump
-	if is_on_floor() and !double_jump_available:
-		double_jump_available = true
+	if is_on_floor() and state == State.JUMPING:
+		_land()
 	
 	# Handle jump.
-	if Input.is_action_just_pressed("jump") and can_jump and (is_on_floor() or double_jump_available):
-		velocity.y = JUMP_VELOCITY
-		if not is_on_floor():
-			double_jump_available = false
+	if Input.is_action_just_pressed("jump") and _can_jump():
+		_jump()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * SPEED
+	if direction and input_enabled:
+		velocity.x = direction * current_speed
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, current_speed)
 	
 	_handle_animation(delta, direction)
 	
 	move_and_slide()
 
 
+func _can_jump() -> bool:
+	return jump_endabled and ((is_on_floor() and state != State.LANDING) or (!is_on_floor() and double_jump_available))
+
+func _jump():
+	state = State.JUMPING
+	velocity.y = JUMP_VELOCITY
+	animation_player.play("jump")
+	if not is_on_floor():
+		double_jump_available = false
+
+func _land():
+	state = State.LANDING
+	if double_jump_enabled:
+		double_jump_available = true
+	current_speed = SPEED/2
+	animation_player.play("land")
+
 func _handle_animation(delta, direction):
 	if direction != 0.0 and velocity.x < 0.0 != sprite.flip_h:
 		sprite.flip_h = !sprite.flip_h
 	
-	if velocity.x != 0.0:
+	if velocity.x != 0.0 and state != State.JUMPING and state != State.LANDING:
 		animation_player.play("walk")
-	elif is_on_floor() and animation_player.is_playing():
-		animation_player.pause()
-	
-func _on_body_entered(_body):
-	hide() # Player disappears after being hit.
-	hit.emit()
-	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true)
+		state = State.WALKING
+	elif is_on_floor() and state != State.LANDING and state != State.JUMPING:
+		state = State.IDLE
+		animation_player.play("idle")
 
-func start(pos):
-	position = pos
+
+func respawn():
 	show()
-	$CollisionShape2D.disabled = false
+	collider.set_deferred("disabled", false)
+	transform.origin = start_position
+	sprite.flip_h = false
+	animation_player.play("idle")
+	state = State.IDLE
+
+
+
+# SIGNALS
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	hide() # Player disappears after being hit.
 	hit.emit()
 	# Must be deferred as we can't change physics properties on a physics callback.
-	$CollisionShape2D.set_deferred("disabled", true) # Replace with function body.
+	collider.set_deferred("disabled", true) # Replace with function body.
+	respawn()
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "land":
+		current_speed = SPEED
+		state = State.IDLE
