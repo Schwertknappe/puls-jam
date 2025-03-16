d169c282863c99702cb5f5a31e103b65366fed9a
Mizzac1 <siebertmaxim1@gmx.de>
2025-03-16 16:40:24 +0100

    UI22

--- scripts/augment_ui.gd
+++ scripts/augment_ui.gd
@@ -1,19 +1,20 @@
 extends CanvasLayer
 signal augment_selected(augment_data)
 
+var ui: Node
 var current_augments: Array = []
 
 func show_augments(augments: Array):
+	
 	current_augments = augments
 	for i in range(3):
 		var panel = $CenterContainer/HBoxContainer.get_child(i)
-		panel.set_augment(augments[i-1])
+		panel.set_augment(augments[i])
 		panel.visible = true
-		
 	visible = true
 
 func _on_augment_selected(augment_data) -> void:
-	Globals.chosen_restrictions.append(augment_data)
+	Globals.chosen_restrictions.append(augment_data)																										
 	visible = false
 	get_tree().paused = false
 	emit_signal("augment_selected", augment_data)
--- scripts/ui.gd
+++ scripts/ui.gd
@@ -1,4 +1,14 @@
 extends Control
+@onready var ui = $UI 
+
+@export var font_paths: Array[String] = [
+	"res://assets/fonts/GlitchGoblin.ttf",
+	"res://assets/fonts/PixelifySans-Bold.ttf",
+	"res://assets/fonts/PixelifySans-VariableFont_wght.ttf",
+	"res://assets/fonts/wingding.ttf"
+]
+@export var min_font_size: int = 10
+@export var max_font_size: int = 40
 
 @onready var augment_ui = $AugmentUI
 @onready var player = $"../Player"
@@ -9,14 +19,25 @@ func _ready():
 	player.hit.connect(update_health_ui)
 	show_augment_selection()
 	get_tree().paused = true
-	$AugmentUI/CenterContainer/HBoxContainer/Augment1/VBoxContainer/Button.grab_focus()
+	$AugmentUI/CenterContainer/HBoxContainer/Augment1/Button.grab_focus()
 
 func show_augment_selection():
 	var augments = AugmentData.get_random_augments(3)
+	augment_ui.ui = $UI
 	augment_ui.show_augments(augments)
 	augment_ui.augment_selected.connect(player._on_augment_selected)
 	player.restriction_added.connect(update_restriction_ui)
-	
+
+	for i in range(3):
+		var panel = $AugmentUI/CenterContainer/HBoxContainer.get_child(i)
+		var title_label = panel.get_node("VBoxContainer/Title")
+		var description_label = panel.get_node("VBoxContainer/Description")
+
+		print("Titel-Label gefunden:", title_label != null)
+		print("Beschreibung-Label gefunden:", description_label != null)
+
+		apply_random_font(title_label, 24)
+		apply_random_font(description_label, 18)
 
 func game_over():
 	get_tree().paused = true
@@ -49,3 +70,13 @@ func _on_game_over_button_pressed():
 	Globals.completed_runs = 0
 	Globals.chosen_restrictions = []
 	get_tree().reload_current_scene()
+	
+func apply_random_font(label: Label, font_size: int):
+	if !label:
+		print("label null")
+		return
+	var new_font = load(font_paths.pick_random())
+	var dynamic_font = new_font.duplicate()
+	
+	label.add_theme_font_override("font", dynamic_font)
+	label.add_theme_font_size_override("font_size", font_size)
