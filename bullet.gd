09e1ee0984da6aca55904a294def55a91c28c072
qIcee <emirkaansari2@gmail.com>
2025-03-16 16:19:12 +0100

    sfx

--- bullet.gd
+++ bullet.gd
@@ -3,6 +3,7 @@ extends Area2D
 var travelled_distance = 0
 var direction: Vector2 = Vector2.RIGHT
 
+@onready var sfx_player = $AudioStreamPlayer2D
 
 func _physics_process(delta: float) -> void:
 	const SPEED = 1000
@@ -20,4 +21,7 @@ func _on_body_entered(body: Node2D) -> void:
 	if body is not Player:
 		queue_free()
 		if body is Enemy:
+			sfx_player.stream = load("res://assets/sfx/enemy_death_sound.wav")
+			sfx_player.play()
 			body.queue_free()
+			
